f634d2a7ac7573e1ee3c905c2b71281bb46d6d24
Natan A. Costa <natanarcosta@gmail.com>
2022-11-05 17:05:26 -0300

    Improving output image

--- src/shared/templates/argos_party_card.template.ts
+++ src/shared/templates/argos_party_card.template.ts
@@ -1,80 +1,112 @@
-const template = `<head>
-<style>
+const template = `<html>
+<head>
+  <style>
     body {
-        height: 320px;
-        border-radius: 15px;
-        width: 600px;
+      height: 320px;
+      width: 600px;
     }
-  .container {
-    display: grid;
-    font-family: Verdana, Geneva, Tahoma, sans-serif;
-  }
-  .card {
-    width: 100%;
-    border-radius: 15px;
-    height: auto;
-    display: flex;
-    justify-content: center;
-    box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
-    background-color: #212121;
-  }
-  .party {
-    width: 50%;
-  }
-  .party ul {
-    list-style-type: none;
-  }
+    .container {
+      display: grid;
+      font-family: Verdana, Geneva, Tahoma, sans-serif;
+    }
 
-  .party ul img {
-    width: 40px;
-  }
+    hr {
+      width: 100%;
+    }
 
-  .party ul li {
-    color: white;
-  }
+    .card {
+      width: 100%;
+      height: auto;
+      display: flex;
+      justify-content: center;
+      box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+      background-color: #212121;
+    }
+    .party {
+      width: 50%;
+      display: flex;
+      justify-content: center;
+    }
 
-  .party ul li div {
-    padding: 1em;
-    display: flex;
-    align-items: center;
-  }
+    .party ul {
+      list-style-type: none;
+      display: flex;
+      justify-items: center;
+      flex-direction: column;
+      width: 100%;
+      padding-right: 3em;
+    }
 
-  .party ul li div span {
-    margin-left: 1em;
-  }
+    .party ul img {
+      width: 40px;
+      height: 40px;
+    }
 
+    .party ul li {
+      color: white;
+    }
 
-  .dps {
-    color: red;
-  }
-  
-</style>
+    .party ul li div {
+      padding: 1em;
+      display: flex;
+      align-items: center;
+    }
+
+    .party ul li div span {
+      margin-left: 1em;
+    }
+
+    .dps {
+      color: red;
+    }
+
+    .debug {
+      border: 1px red solid;
+    }
+  </style>
 </head>
 <body>
-<div class="container">
-  <div class="card">
-    <div class="party">
-      <ul>
-         <li><div><img src=%IMAGE1%> <span class="dps"> %DPS1%</span> </div></li>
-         <hr>
-         <li><div><img src=%IMAGE3%> <span> %ALT1%</span> </div></li>
-         <li><div><img src=%IMAGE4%> <span> %ALT2%</span> </div></li>
-         <li><div><img src=%IMAGE5%> <span> %ALT3%</span> </div></li>
-      </ul>
-    </div>
-    <div class="party">
-      <ul>
-        <li><div><img src=%IMAGE2%> <span class="dps"> %DPS2%</span> </div></li>
-        <hr>
-        <li><div><img src=%IMAGE6%> <span> %ALT4%</span> </div></li>
-        <li><div><img src=%IMAGE7%> <span> %ALT5%</span> </div></li>
-        <li><div><img src=%IMAGE8%> <span> %ALT6%</span> </div></li>
-      </ul>
+  <div class="container">
+    <div class="card">
+      <div class="party">
+        <ul>
+          <li>
+            <div><img src="%IMAGE1%" /> <span class="dps"> %DPS1%</span></div>
+          </li>
+          <hr />
+          <li>
+            <div><img src="%IMAGE3%" /> <span> %ALT1%</span></div>
+          </li>
+          <li>
+            <div><img src="%IMAGE4%" /> <span> %ALT2%</span></div>
+          </li>
+          <li>
+            <div><img src="%IMAGE5%" /> <span> %ALT3%</span></div>
+          </li>
+        </ul>
+      </div>
+      <div class="party">
+        <ul>
+          <li>
+            <div><img src="%IMAGE2%" /> <span class="dps"> %DPS2%</span></div>
+          </li>
+          <hr />
+          <li>
+            <div><img src="%IMAGE6%" /> <span> %ALT4%</span></div>
+          </li>
+          <li>
+            <div><img src="%IMAGE7%" /> <span> %ALT5%</span></div>
+          </li>
+          <li>
+            <div><img src="%IMAGE8%" /> <span> %ALT6%</span></div>
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
-</div>
 </body>
 </html>
+
 `;
 export interface CreateImage {
   dps1: {
